7635fb82c98008d54df1789219adaa1a280cd14e
Colin Rofls <colin@cmyr.net>
2019-02-26 20:06:50 -0500

    Break it all again :/

--- pars-derive/src/lib.rs
+++ pars-derive/src/lib.rs
@@ -33,6 +33,24 @@
 //!     y: f64,
 //! }
 //! ```
+//!
+//! Given input like "unit", "struct hello 5", "tuple hello 4"
+//!
+//! ```ignore
+//! #[pars::fmt("#_: (#type)")] // sketchy af
+//! #[pars::re(fn = ThreeKinds::get_type)]
+//! #[pars::re(r"(\w_).*"), Uppercase]
+//! enum ThreeKinds {
+//!     Unit,
+//!     #[pars(re(r"\w+ (\w+) (\w+)")]
+//!     Struct {
+//!        name: String,
+//!        size: usize,
+//!     },
+//!     #[pars(re(r"\w+ (\w+) (\w+)")]
+//!     Tuple(String, usize),
+//! }
+//! ```
 
 extern crate proc_macro;
 #[macro_use]
@@ -42,7 +60,7 @@ extern crate quote;
 
 mod container;
 
-use container::{Container, Data, Field};
+use container::{Container, Data, Field, Style};
 use proc_macro::TokenStream as TokenStream1;
 use proc_macro2::{Span, TokenStream};
 use std::collections::HashMap;
@@ -52,22 +70,33 @@ use syn::{
     NestedMeta,
 };
 
+enum Mode {
+    Fmt(AttributeArgs),
+    Regex(AttributeArgs),
+}
+
+/// Returns the format/re string, e.g. for #[pars::fmt("my string")] returns
 #[proc_macro_attribute]
 pub fn re(attr: TokenStream1, tokens: TokenStream1) -> TokenStream1 {
-    //ensure_no_extra_attrs(&ast.attrs);
-    let parse_tokens = tokens.clone();
     let args = parse_macro_input!(attr as AttributeArgs);
-    let item = parse_macro_input!(parse_tokens as DeriveInput);
-    let re_string = get_attr_string(args);
+    let mode = Mode::Regex(args);
+
+    let item = parse_macro_input!(tokens as DeriveInput);
 
     let container = Container::from_ast(&item).unwrap();
 
-    let generated = match &container.data {
-        Data::Enum(_) => unimplemented!(),
-        Data::Struct(_, _) => gen_re_struct(re_string, &container),
-    };
+    generate_impls(mode, &container)
+        .unwrap_or_else(to_compile_errors)
+        .into()
 
-    tokens.into_iter().chain(generated).collect()
+    //let re_string = get_attr_string(args);
+
+    //let generated = match &container.data {
+        //Data::Enum(_) => unimplemented!(),
+        //Data::Struct(_, _) => gen_re_struct(re_string, &container),
+    //};
+
+    //tokens.into_iter().chain(generated).collect()
 }
 
 #[proc_macro_attribute]
@@ -79,6 +108,51 @@ pub fn fmt(_attr: TokenStream1, _tokens: TokenStream1) -> TokenStream1 {
     //ensure_no_extra_attrs(&ast.attrs);
     //eprintln!("token attrs: {:?}", ast.attrs);
     TokenStream1::new()
+}
+
+/// The 'main' function for the actual code generation
+fn generate_impls(mode: Mode, cont: &Container) -> Result<TokenStream, Vec<syn::Error>> {
+
+    let ident = &cont.ident;
+    let orig = &cont.original;
+
+    let body = match &cont.data {
+        Data::Struct(Style::Struct, ref fields) => gen_struct_body(&mode, fields),
+        Data::Struct(Style::Tuple, ref fields) => gen_tuple_body(&mode, fields),
+        _other => unimplemented!(),
+    };
+
+    let impl_block = quote! {
+        #orig
+
+        impl #ident {
+            fn pars_from_str(src: &str) -> Result<Self, String> {
+                #body
+            }
+        }
+    };
+    Ok(impl_block)
+}
+
+//NOTE: JUST A SKECTH
+//trait Matcher {
+    //fn get_match<T: FromStr>(&self, idx: usize) -> Result<T, T::Err>;
+//}
+
+fn gen_struct_body<'a>(mode: &Mode, fields: &[Field<'a>]) -> TokenStream {
+    unimplemented!()
+        //let mut tokes = TokenStream::new();
+        //for (i, f) in fields.iter().enumerate() {
+            //let name = &f.ident;
+
+            //tokes.extend(quote! {
+                //#name: matches.get_match(#i).cool()??,
+            //})
+        //}
+}
+
+fn gen_tuple_body<'a>(mode: &Mode, fields: &[Field<'a>]) -> TokenStream {
+    unimplemented!()
 }
 
 //FIXME: static regex or something, at least don't be compiling in the parse fn
@@ -162,7 +236,73 @@ fn ensure_no_extra_attrs(attrs: &[syn::Attribute]) {
     })
 }
 
-/// Returns the format/re string, e.g. for #[pars::fmt("my string")] returns
+//#[proc_macro_derive(ParsFromStr, attributes(pars))]
+//pub fn pars_from_str(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    //let ast: syn::DeriveInput = syn::parse(tokens).unwrap();
+    //let pars_fmt = match extract_meta2(&ast) {
+        //Ok(Mode::Fmt(s)) => s,
+        //Ok(Mode::Regex { .. }) => {
+            //eprintln!("pars::re doesn't actually do anything yet");
+            //return proc_macro::TokenStream::new();
+        //}
+        //Err(e) => panic!("{}", e),
+    //};
+
+    //let (variables, nonvariables) = parse_vars(&pars_fmt);
+    //let struct_ident = ast.ident;
+
+    //let function_body = {
+        //let mut function_body = TokenStream::new();
+        //assert_eq!(nonvariables.len(), variables.len() + 1);
+
+        //let mut nonvariables = nonvariables.iter();
+
+        //for variable in &variables {
+            //let nonvariable = nonvariables.next().unwrap();
+            //let variable = Ident::new(variable, Span::call_site());
+
+            //function_body.extend(quote! {
+                //assert!(string.starts_with(#nonvariable));
+                //let (_, string) = string.split_at(#nonvariable.len());
+                //let split_index = string.find(|c: char| !c.is_alphanumeric())
+                    //.unwrap_or(string.len());
+                //let (#variable, string) = string.split_at(split_index);
+            //});
+        //}
+
+        //let last_nonvariable = nonvariables.next();
+        //function_body.extend(quote! {
+            //assert_eq!(string, #last_nonvariable);
+        //});
+
+        //function_body
+    //};
+
+    //let struct_initializer_body = {
+        //let mut struct_initializer_body = TokenStream::new();
+        //for variable in variables {
+            //let variable = Ident::new(variable, Span::call_site());
+            //struct_initializer_body.extend(quote! {
+                //#variable : #variable.parse().unwrap(),
+            //});
+        //}
+
+        //struct_initializer_body
+    //};
+
+    //let gen = quote! {
+        //fn our_fun_function(string: &str) -> #struct_ident {
+
+            //#function_body
+
+            //#struct_ident{
+                //#struct_initializer_body
+            //}
+        //}
+    //};
+    //gen.into()
+//}
+
 /// "my string".
 fn get_attr_string(args: AttributeArgs) -> String {
     match args.first() {
@@ -172,121 +312,48 @@ fn get_attr_string(args: AttributeArgs) -> String {
     }
 }
 
-#[proc_macro_derive(ParsFromStr, attributes(pars))]
-pub fn pars_from_str(tokens: proc_macro::TokenStream) -> proc_macro::TokenStream {
-    let ast: syn::DeriveInput = syn::parse(tokens).unwrap();
-    let pars_fmt = match extract_meta2(&ast) {
-        Ok(Mode::Fmt(s)) => s,
-        Ok(Mode::Regex { .. }) => {
-            eprintln!("pars::re doesn't actually do anything yet");
-            return proc_macro::TokenStream::new();
-        }
-        Err(e) => panic!("{}", e),
-    };
-
-    let (variables, nonvariables) = parse_vars(&pars_fmt);
-    let struct_ident = ast.ident;
-
-    let function_body = {
-        let mut function_body = TokenStream::new();
-        assert_eq!(nonvariables.len(), variables.len() + 1);
-
-        let mut nonvariables = nonvariables.iter();
-
-        for variable in &variables {
-            let nonvariable = nonvariables.next().unwrap();
-            let variable = Ident::new(variable, Span::call_site());
-
-            function_body.extend(quote! {
-                assert!(string.starts_with(#nonvariable));
-                let (_, string) = string.split_at(#nonvariable.len());
-                let split_index = string.find(|c: char| !c.is_alphanumeric())
-                    .unwrap_or(string.len());
-                let (#variable, string) = string.split_at(split_index);
-            });
-        }
-
-        let last_nonvariable = nonvariables.next();
-        function_body.extend(quote! {
-            assert_eq!(string, #last_nonvariable);
-        });
-
-        function_body
-    };
-
-    let struct_initializer_body = {
-        let mut struct_initializer_body = TokenStream::new();
-        for variable in variables {
-            let variable = Ident::new(variable, Span::call_site());
-            struct_initializer_body.extend(quote! {
-                #variable : #variable.parse().unwrap(),
-            });
-        }
-
-        struct_initializer_body
-    };
-
-    let gen = quote! {
-        fn our_fun_function(string: &str) -> #struct_ident {
-
-            #function_body
-
-            #struct_ident{
-                #struct_initializer_body
-            }
-        }
-    };
-    gen.into()
-}
-
-/// Macro operating modes, parsed from macro attributes.
-enum Mode {
-    Fmt(String),
-    Regex { pattern: String, group_order: Option<Vec<String>> },
-}
-
 const META_RE: &str = "pars::re";
 const META_FMT: &str = "pars::fmt";
 
-fn extract_meta2(ast: &syn::DeriveInput) -> Result<Mode, String> {
-    let mut all_args = ast
-        .attrs
-        .iter()
-        .inspect(|a| eprintln!("{:?}", a.path))
-        .flat_map(|a| a.parse_meta())
-        .flat_map(|meta_attr| match meta_attr {
-            Meta::List(l) => {
-                eprintln!("list {:?}", l);
-                None
-            }
-            Meta::Word(w) => {
-                eprintln!("word {:?}", w);
-                None
-            }
-            Meta::NameValue(MetaNameValue { ident, lit, .. }) => {
-                eprintln!("nameval {:?}: {:?}", &ident, &lit);
-                Some((ident.to_string(), lit))
-            }
-        })
-        .collect::<HashMap<_, _>>();
-    println!("all args: {:?}", &all_args.keys().collect::<Vec<_>>());
+//fn extract_meta2(ast: &syn::DeriveInput) -> Result<Mode, String> {
+    //let mut all_args = ast
+        //.attrs
+        //.iter()
+        //.inspect(|a| eprintln!("{:?}", a.path))
+        //.flat_map(|a| a.parse_meta())
+        //.flat_map(|meta_attr| match meta_attr {
+            //Meta::List(l) => {
+                //eprintln!("list {:?}", l);
+                //None
+            //}
+            //Meta::Word(w) => {
+                //eprintln!("word {:?}", w);
+                //None
+            //}
+            //Meta::NameValue(MetaNameValue { ident, lit, .. }) => {
+                //eprintln!("nameval {:?}: {:?}", &ident, &lit);
+                //Some((ident.to_string(), lit))
+            //}
+        //})
+        //.collect::<HashMap<_, _>>();
+    //println!("all args: {:?}", &all_args.keys().collect::<Vec<_>>());
 
-    if all_args.contains_key(META_FMT) && all_args.contains_key(META_RE) {
-        Err("Only one of #[pars::fmt] or #[pars::re] can be provided.".into())
-    } else if let Some(lit) = all_args.remove(META_RE) {
-        match lit {
-            Lit::Str(s) => Ok(Mode::Regex { pattern: s.value(), group_order: None }),
-            _other => Err("pars::re failed, expected str found... something else".into()),
-        }
-    } else if let Some(lit) = all_args.remove(META_FMT) {
-        match lit {
-            Lit::Str(s) => Ok(Mode::Fmt(s.value())),
-            _other => Err("pars::fmt failed, expected str found... something else".into()),
-        }
-    } else {
-        Err("`#[derive(ParsFromStr)]` requires one of `#[pars::fmt]` or #[pars::re].".into())
-    }
-}
+    //if all_args.contains_key(META_FMT) && all_args.contains_key(META_RE) {
+        //Err("Only one of #[pars::fmt] or #[pars::re] can be provided.".into())
+    //} else if let Some(lit) = all_args.remove(META_RE) {
+        //match lit {
+            //Lit::Str(s) => Ok(Mode::Regex { pattern: s.value(), group_order: None }),
+            //_other => Err("pars::re failed, expected str found... something else".into()),
+        //}
+    //} else if let Some(lit) = all_args.remove(META_FMT) {
+        //match lit {
+            //Lit::Str(s) => Ok(Mode::Fmt(s.value())),
+            //_other => Err("pars::fmt failed, expected str found... something else".into()),
+        //}
+    //} else {
+        //Err("`#[derive(ParsFromStr)]` requires one of `#[pars::fmt]` or #[pars::re].".into())
+    //}
+//}
 
 /// Returns
 fn extract_meta(ast: &syn::DeriveInput) -> Option<String> {
@@ -339,6 +406,11 @@ fn parse_vars(s: &str) -> (Vec<&str>, Vec<&str>) {
     }
 
     (variables, nonvariables)
+}
+
+fn to_compile_errors(errors: Vec<syn::Error>) -> proc_macro2::TokenStream {
+    let compile_errors = errors.iter().map(syn::Error::to_compile_error);
+    quote!(#(#compile_errors)*)
 }
 
 #[cfg(test)]
