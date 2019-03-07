a1e3bb43efee608d2decca0f7be31946ed29c6c6
Galen O'Sullivan <galenosullivan@gmail.com>
2019-03-07 14:47:30 -0500

    Add gen_from_str option to generate FromStr

--- pars/tests/basic-derive.rs
+++ pars/tests/basic-derive.rs
@@ -33,3 +33,35 @@ fn fmt_parse_smoke_test() {
     let not_rect = Rect::pars_from_str("4, 5, (420 808)");
     assert!(not_rect.is_err());
 }
+
+#[test]
+fn fmt_derive_from_str() {
+    #[pars::fmt("(#{x}, #{y}) (#{width}, #{height})", gen_from_str)]
+    #[derive(Debug, PartialEq)]
+    struct Rect {
+        x: usize,
+        y: usize,
+        width: usize,
+        height: usize,
+    }
+
+    let rect: Rect = "(4, 10) (42, 69)".parse().unwrap();
+    assert_eq!(rect, Rect { x: 4, y: 10, width: 42, height: 69 });
+
+    let not_rect = Rect::pars_from_str("4, 5, (420 808)");
+    assert!(not_rect.is_err());
+}
+
+#[test]
+fn re_derive_from_str() {
+    #[pars::re(r"(\w+): \(([+\-\d]+), ([+\-\d]+)\)", gen_from_str)]
+    #[derive(Debug, PartialEq)]
+    struct NamedPos2 {
+        name: String,
+        x: isize,
+        y: isize,
+    }
+
+    let named_pos: NamedPos2 = "hello: (32, -420)".parse().unwrap();
+    assert_eq!(named_pos, NamedPos2 { name: "hello".into(), x: 32, y: -420 });
+}
