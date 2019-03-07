ae1641d6cbbfedb6b877103bd8ec9faded47a1e0
Galen O'Sullivan <galenosullivan@gmail.com>
2019-03-07 15:01:13 -0500

    Move pars-test to pars/tests
    It just seems cleaner, I guess.

--- pars/tests/basic-derive.rs
+++ pars/tests/basic-derive.rs
@@ -1,14 +1,6 @@
 use pars;
 use pars::ParsFromStr;
 
-//#[pars::fmt("$name: ($x, $y)")]
-//#[derive(Debug, PartialEq)]
-//struct NamedPos {
-//name: String,
-//x: isize,
-//y: isize,
-//}
-
 #[pars::re(r"(\w+): \(([+\-\d]+), ([+\-\d]+)\)")]
 #[derive(Debug, PartialEq)]
 struct NamedPos2 {
@@ -20,14 +12,4 @@ struct NamedPos2 {
 fn main() {
     let named_pos = NamedPos2::pars_from_str("hello: (32, -420)").unwrap();
     assert_eq!(named_pos, NamedPos2 { name: "hello".into(), x: 32, y: -420 });
-    println!("success");
-}
-
-#[cfg(test)]
-mod tests {
-    use super::main;
-    #[test]
-    fn test() {
-        main()
-    }
 }
